657f2428a8fff8c31c7b913fe9c47d6f4fa51a54
poonu <poonu@code with poonu.com>
2026-09-05 13:25:57 +0530

    better approach for the three sum

--- Arrays/threesum.java
+++ Arrays/threesum.java
@@ -4,19 +4,23 @@ class Solution {
         int n = nums.length;
 
         for (int i = 0; i < n; i++) {
+            Set<Integer> hashset = new HashSet<>();
             for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
 
-                        Collections.sort(temp);
+                int third = -(nums[i] + nums[j]);
 
-                        set.add(temp);
-                    }
+                if(hashset.contains(third)) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(third);
+
+                    Collections.sort(temp);
+
+                    set.add(temp);
                 }
+
+                hashset.add(nums[j]);
             }
         }
         return new ArrayList<>(set);
